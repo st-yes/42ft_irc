e0af0ba3d914e67edfe00fc93976ac181e88ef84
Oussama Ouazahrou <oouazahr@e2r10p1.1337.ma>
2023-07-05 03:01:15 +0100

    probably the last version

--- src/JoinAndPart.cpp
+++ src/JoinAndPart.cpp
@@ -152,7 +152,6 @@ void    Server::JoinFunc(std::unordered_map<std::string, std::string>   tmp, Use
         if (Chanl == NULL)
             this->newChannel(p, Userx);
         else{
-            User *chanOp = this->findUserinChan(Userx->sendFd, Chanl->channelOps);
             if (Chanl->inviteMode && !channelFinder(Chanl->channelName, Userx->invitedChannels)
                 && findUserinChan(Userx->sendFd, Chanl->channelOps) == -1){
                 this->sendHermes(this->sendNumericCode(Userx, Chanl, ERR_INVITEONLYCHAN, ""), send);
